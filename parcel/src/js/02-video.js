34e9d13880db1e49d2c16f12f089174302333d57
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-20 01:43:02 +0200

    task 2

--- parcel/src/js/02-video.js
+++ parcel/src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);  
@@ -7,6 +8,22 @@ player.on('play', function() {
     console.log('played the video!');
 });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+player.on('timeupdate', function(e) {
+    console.log(e.seconds); 
 });
+player.on('timeupdate', function(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+    player.setCurrentTime(savedTime).then(function(seconds) {
+       
+    }).catch(function(error) {
+    
+    });
+}
+
+player.on('timeupdate', throttle(function(event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000)); 
